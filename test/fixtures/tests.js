5204094f015a70adc78ea3ddf6d94f7f62254529
Jasper Woudenberg <mail@jasperwoudenberg.com>
2016-03-02 21:28:19 +0100

    Added first set of unit tests.

---
+++ test/fixtures/tests.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var Test = require('../../lib/test');
+
+exports.succeeding = Test(
+  'succeeding test',
+  {},
+  function () {
+    assert(true);
+  }
+);
+
+exports.failing = Test(
+  'failing test',
+  {},
+  function () {
+    throw new Error('failed');
+  }
+);
+
+exports.returning = Test(
+  'returning test',
+  {},
+  function () {
+    return 42;
+  }
+);
+
+exports.requiring = Test(
+  'requiring test',
+  { answer: exports.returning },
+  function (result) {
+    assert.equal(result.answer, 42);
+  }
+);
---
+++ test/test.js
@@ -0,0 +1,36 @@
+var test = require('tap').test;
+var sampleTests = require('./fixtures/tests');
+
+
+test('Succeeding test returns a future that will resolve', function (t) {
+  var test = sampleTests.succeeding;
+  test.fork(t.notOk, t.end);
+});
+
+
+test('Failing test returns a future that rejects with the failure error', function (t) {
+  var test = sampleTests.failing;
+  test.fork(
+    function (error) {
+      t.type(error, 'Error');
+      t.equal(error.message, 'failed');
+    },
+    t.notOk
+  );
+  t.end();
+});
+
+
+test('Returning test returns a promise that resolves with the returned value', function (t) {
+  var test = sampleTests.returning;
+  test.fork(t.notOk, function (result) {
+    t.equal(result, 42);
+    t.end();
+  });
+});
+
+
+test('Test can require other tests', function (t) {
+  var test = sampleTests.requiring;
+  test.fork(t.notOk, t.end);
+});
