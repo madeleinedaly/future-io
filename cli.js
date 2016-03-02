2921fbb030a4927743a0a102f7f7487808ed955b
Jasper Woudenberg <mail@jasperwoudenberg.com>
2016-03-02 20:46:09 +0100

    Put all IO in cli.js.
    All other files export pure functions and should be nicely
    unit-testable.

--- cli.js
+++ cli.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
+var R = require('ramda');
 var path = require('path');
 var minimist = require('minimist');
-var runner = require('./lib/runner');
+var run = require('./lib/runner');
+var report = require('./lib/reporter');
 
 var argv = minimist(process.argv.slice(2));
 
-const file = path.join(process.cwd(), argv._[0]);
+var file = path.join(process.cwd(), argv._[0]);
+var runFromPath = R.compose(run, loadTests);
 
-runner(file);
+runFromPath(file)
+  .map(report)
+  .fork(onError, onCompletion);
+
+function onError (err) {
+  console.log('Macho imploded');
+  console.log(err);
+}
+
+function onCompletion (output) {
+  console.log(output);
+}
+
+function loadTests (path) {
+  var tests = R.values(require(path));
+  return tests;
+}
