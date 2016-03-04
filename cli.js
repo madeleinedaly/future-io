8f60a2d4430083233df3e7640b0231d17590acf9
Stoeffel <schtoeffel@gmail.com>
2016-03-04 12:30:16 +0100

    introduce IO

--- cli.js
+++ cli.js
@@ -6,30 +6,27 @@ var minimist = require('minimist');
 var fork = require('fork-future');
 var run = require('./lib/runner');
 var report = require('./lib/reporter');
+var io = require('./lib/io');
 
 var argv = minimist(process.argv.slice(2));
 
 var file = path.join(process.cwd(), argv._[0]);
 var runFromPath = R.compose(run, loadTests);
 
+function loadTests (path) {
+  var tests = R.values(require(path));
+  return tests;
+}
+
+// UNSAFECODE
+// ===============================
 var cli = R.compose(
-  fork(onError, onCompletion),
+  fork(
+    R.compose(io.runIO, io.errorIO),
+    R.compose(io.runIO, io.logIO)
+  ),
   R.map(report),
   runFromPath
 );
 
 cli(file);
-
-function onError (err) {
-  console.log('Macho imploded');
-  console.log(err);
-}
-
-function onCompletion (output) {
-  console.log(output);
-}
-
-function loadTests (path) {
-  var tests = R.values(require(path));
-  return tests;
-}
