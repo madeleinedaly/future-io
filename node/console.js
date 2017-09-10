3c513f251160167fd01fa4f3450e5a6b4aa56d26
Madeleine Daly <mdaly@lifeimage.com>
2017-09-09 21:20:10 -0400

    enable alternate Task implementation injection in node/console

--- node/console.js
+++ node/console.js
@@ -1,6 +1,20 @@
-const wrapFunction = require('../lib/wrap-function')
+const _wrapFunction = require('../lib/wrap-function')
 
-const consoleMethods = ['assert', 'dir', 'error', 'info', 'log', 'time', 'timeEnd', 'trace', 'warn']
-consoleMethods.forEach(
-  name => { exports[name] = wrapFunction('console.' + name, console[name]) }
-)
+function create (Task, methods) {
+
+  const wrapFunction = Task ? _wrapFunction.create(Task, methods) : _wrapFunction
+
+  const consoleMethods = [
+    'assert', 'dir', 'error', 'info', 'log',
+    'time', 'timeEnd', 'trace', 'warn'
+  ]
+
+  return consoleMethods.reduce((wrappedMethods, name) => {
+    wrappedMethods[name] = wrapFunction('console.' + name, console[name])
+    return wrappedMethods
+  }, {})
+
+}
+
+module.exports = create()
+module.exports.create = create
